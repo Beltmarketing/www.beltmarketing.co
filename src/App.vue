cac223c3e90a6f5278cd7c3ae07447a6a263e04f
jhord <jhordymondragon4@gmail.com>
2025-02-24 10:11:03 -0500

    Analitica vercel

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Header from "@/components/Header.vue";
 import CustomCursor from "@/components/CustomCursor.vue";
+import { Analytics } from '@vercel/analytics/vue';
 </script>
 
 <template>
+  <Analytics/>
   <router-view ></router-view>
   <CustomCursor/>
 </template>
